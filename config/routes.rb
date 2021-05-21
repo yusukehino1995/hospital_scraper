f0278cf67bcf0a30ee335ea8e0b72efa01d7d07c
樋野雄介 <hinosuke@hinosuke.local>
2021-05-21 18:55:04 +0900

    fix_rubocop_files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root to: 'hospitals#index'
-  resources  :hospitals
+  resources :hospitals
 end
--- spec/requests/hospitals_spec.rb
+++ spec/requests/hospitals_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Hospitals", type: :request do
-  describe "GET /hospitals" do
-    it "response success" do
+RSpec.describe 'Hospitals', type: :request do
+  describe 'GET /hospitals' do
+    it 'response success' do
       get hospitals_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
     end
   end
 end
