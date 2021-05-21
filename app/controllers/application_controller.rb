e2a134e76d3abd5ecf5a16678c20a20b09599717
樋野雄介 <hinosuke@hinosuke.local>
2021-05-21 19:00:54 +0900

    change_require_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  require 'csv'
+  require 'open-uri'
+  require 'nokogiri'
 end
--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class HospitalsController < ApplicationController
-  require 'csv'
-  require 'open-uri'
-  require 'nokogiri'
-
   def index
     respond_to do |format|
       format.html
